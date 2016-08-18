ebc8abad3903a67a1a0cbd777539109c255d6326
Joshua Westerheide <dev@jdoubleu.de>
2016-08-18 22:55:35 +0200

    Init sass compiling by requiring sass (and autoprefixer) as gulp modules, settings up gulp tasks, creating a main sass file/stylesheet file and a settings file and adding new paths to gitignore.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@
 let gulp = require('gulp');
 let sourcemaps = require("gulp-sourcemaps");
 let babel = require("gulp-babel");
+let autoprefixer = require('gulp-autoprefixer');
+let sass = require('gulp-sass');
 
 // Constants
 const dirs = {
@@ -38,4 +40,15 @@ gulp.task("compile:javascript:es6", function () {
 		})
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dirs.assets.dist.scripts));
+});
+
+gulp.task('compile:styles:scss', function() {
+    return gulp.src(dirs.assets.src.styles + '/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(autoprefixer({
+        	browsers: ['last 2 versions', 'IE 9']
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dirs.assets.dist.styles));
 });
