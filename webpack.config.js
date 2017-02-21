bee10b4c3c422beb2be427ecf499eae9a7cbbaa3
Joshua Westerheide <dev@jdoubleu.de>
2017-02-21 20:30:06 +0100

    Added basic web pack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/**
+ * Frontend Boilerplate
+ * Webpack configuration file
+ *
+ * This file will configure and set up and development environment using Webpack (https://webpack.js.org/)
+ * while offering build flavors für production.
+ *
+ * This config is written in ES6 so you need NodeJS 6.4 or higher.
+ */
+
+// System Includes
+let path = require('path');
+
+// Tools Includes
+let webpack = require('webpack');
+
+// Webpack configuration
+module.exports = {
+	entry: './assets/src/scripts/main.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'assets/dist/scripts/')
+	}
+};
