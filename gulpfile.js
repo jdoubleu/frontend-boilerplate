6927bf6f2ef8c474ab7db4d1a3189aaa723d2b04
Joshua Westerheide <dev@jdoubleu.de>
2017-03-30 14:31:49 +0200

    Dropped specific compilation directory, so that the sources structure will be preserved

--- gulpfile.js
+++ gulpfile.js
@@ -16,16 +16,8 @@ let pug = require('gulp-pug');
 // Constants
 const dirs = {
 	assets: {
-		src: {
-			styles: 'assets/src/styles',
-			scripts: 'assets/src/scripts',
-			templates: 'assets/src/templates'
-		},
-		dist: {
-			styles: 'assets/dist/styles',
-			scripts: 'assets/dist/scripts',
-			templates: 'assets/dist/templates'
-		},
+		src: 'assets/src',
+		dist: 'assets/dist',
 		vendor: 'assets/vendor/'
 	}
 };
@@ -34,7 +26,7 @@ const dirs = {
 
 // # Compile tasks
 gulp.task('compile:javascript:es6', () => {
-	return gulp.src(dirs.assets.src.scripts + '/**/*.js')
+	return gulp.src(dirs.assets.src + '/**/*.js')
 		.pipe(sourcemaps.init())
 		.pipe(babel({
 			presets: ['es2015']
@@ -44,24 +36,24 @@ gulp.task('compile:javascript:es6', () => {
 			this.emit('end');
 		})
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(dirs.assets.dist.scripts));
+		.pipe(gulp.dest(dirs.assets.dist));
 });
 
 gulp.task('compile:styles:scss', () => {
-    return gulp.src(dirs.assets.src.styles + '/**/*.scss')
+    return gulp.src(dirs.assets.src + '/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass()).on('error', sass.logError)
         .pipe(autoprefixer({
         	browsers: ['last 2 versions', 'IE 9']
         }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(dirs.assets.dist.styles));
+        .pipe(gulp.dest(dirs.assets.dist));
 });
 
 gulp.task('compile:templates:pug', () => {
-	return gulp.src(dirs.assets.src.templates + '/**/*.pug')
+	return gulp.src(dirs.assets.src + '/**/*.pug')
 		.pipe(pug())
-		.pipe(gulp.dest(dirs.assets.dist.templates));
+		.pipe(gulp.dest(dirs.assets.dist));
 });
 
 gulp.task('compile', ['compile:javascript:es6', 'compile:styles:scss', 'compile:templates:pug']);
@@ -69,21 +61,21 @@ gulp.task('compile', ['compile:javascript:es6', 'compile:styles:scss', 'compile:
 // # Build tasks
 gulp.task('build:javascript', ['compile:javascript:es6'], () => {
 	let uglify = require('gulp-uglify');
-    return gulp.src(dirs.assets.dist.scripts + "/**/*.js")
+    return gulp.src(dirs.assets.dist + "/**/*.js")
         .pipe(uglify({
 			preserveComments: 'license'
 		}).on('error', function(e) {
           console.error(e);
           this.emit('end');
         }))
-        .pipe(gulp.dest(dirs.assets.dist.scripts));
+        .pipe(gulp.dest(dirs.assets.dist));
 });
 
 gulp.task('build:styles', ['compile:styles:scss'], () => {
 	let uglify = require('gulp-uglifycss');
-    return gulp.src(dirs.assets.dist.styles + '/**/*.css')
+    return gulp.src(dirs.assets.dist + '/**/*.css')
 		.pipe(uglify())
-		.pipe(gulp.dest(dirs.assets.dist.styles));
+		.pipe(gulp.dest(dirs.assets.dist));
 });
 
 gulp.task('build:templates', ['compile:templates:pug']);
@@ -93,14 +85,14 @@ gulp.task('build', ['build:javascript', 'build:styles', 'build:templates']);
 // # Linting tasks
 gulp.task('lint:javascript:es6', () => {
 	let eslint = require('gulp-eslint');
-	return gulp.src([dirs.assets.src.scripts + '/**/*.js'])
+	return gulp.src([dirs.assets.src + '/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format());
 });
 
 gulp.task('lint:styles:scss', () => {
 	let stylelint = require('gulp-stylelint');
-	return gulp.src([dirs.assets.src.styles + '/**/*.scss'])
+	return gulp.src([dirs.assets.src + '/**/*.scss'])
         .pipe(stylelint({
         	failAfterError: false,
 			reporters: [
@@ -113,14 +105,14 @@ gulp.task('lint', ['lint:javascript:es6', 'lint:styles:scss']);
 
 // # Watcher tasks
 gulp.task('watch:compile', ['compile'], () => {
-	gulp.watch(dirs.assets.src.styles + '/**/*.scss', ['compile:styles:scss']);
-    gulp.watch(dirs.assets.src.scripts + '/**/*.js', ['compile:javascript:es6']);
-    gulp.watch(dirs.assets.src.templates + '/**/*.pug', ['compile:templates:pug']);
+	gulp.watch(dirs.assets.src + '/**/*.scss', ['compile:styles:scss']);
+    gulp.watch(dirs.assets.src + '/**/*.js', ['compile:javascript:es6']);
+    gulp.watch(dirs.assets.src + '/**/*.pug', ['compile:templates:pug']);
 });
 
 gulp.task('watch:lint', ['lint'], () => {
-	gulp.watch(dirs.assets.src.styles + '/**/*.scss', ['lint:styles:scss']);
-    gulp.watch(dirs.assets.src.scripts + '/**/*.js', ['lint:javascript:es6']);
+	gulp.watch(dirs.assets.src + '/**/*.scss', ['lint:styles:scss']);
+    gulp.watch(dirs.assets.src + '/**/*.js', ['lint:javascript:es6']);
 });
 
 gulp.task('watch', ['watch:lint', 'watch:compile']);
