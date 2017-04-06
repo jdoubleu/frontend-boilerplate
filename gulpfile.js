81ddae3bb9e9c413ace461730562e52f4ee921bc
Joshua Westerheide <dev@jdoubleu.de>
2017-04-06 20:02:52 +0200

    Cleaned up tasks a bit more. Adapting gulp-v4 suggested syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
 'use strict';
 
-/**
- * Project gulpfile
- * Adds tasks to compile, lint and minify styles, scripts, etc.
- */
-
 // Requirements
 let gulp = require('gulp');
 let sourcemaps = require("gulp-sourcemaps");
 let babel = require("gulp-babel");
+let eslint = require('gulp-eslint');
 let autoprefixer = require('gulp-autoprefixer');
 let sass = require('gulp-sass');
+let stylelint = require('gulp-stylelint');
 let pug = require('gulp-pug');
 let argv = require('yargs').argv;
 let del = require('del');
 
 // Constants
-const prod = process.env.NODE_ENV === 'production' || argv.production || argv.prod;
-const dirs = {
-	assets: {
-		src: 'assets/src',
-		dist: 'assets/dist',
-		vendor: 'assets/vendor/'
-	}
+const PRODUCTION = process.env.NODE_ENV === 'production' || argv.production || argv.prod;
+const PATHS = {
+	src: 'assets/src',
+	dest: 'assets/dist'
+};
+const FILES = {
+	es6: '/**/*.js',
+	scss: '/**/*.scss',
+	pug: '/**/*.pug'
 };
 
 // Tasks
 // # General tasks
-gulp.task('clean', () => {
-	return del([dirs.assets.dist]);
-});
+function clean() {
+	return del([PATHS.dest]);
+}
 
-// # Compile tasks
-function compileES6() {
-	let stream = gulp.src(dirs.assets.src + '/**/*.js')
+// # Compiler functions
+function scripts() {
+	let stream = gulp.src(PATHS.src + FILES.es6)
 		.pipe(sourcemaps.init())
 		.pipe(babel({
 			presets: ['es2015']
@@ -43,7 +42,7 @@ function compileES6() {
 			this.emit('end');
 		});
 
-	if(prod) {
+	if(PRODUCTION) {
 		let uglify = require('gulp-uglify');
 		stream.pipe(uglify({
 				preserveComments: 'license'
@@ -54,88 +53,72 @@ function compileES6() {
 	} else
 		stream.pipe(sourcemaps.write('./'));
 
-	return stream.pipe(gulp.dest(dirs.assets.dist));
+	return stream.pipe(gulp.dest(PATHS.dest));
 }
 
-gulp.task('compile:javascript:es6', compileES6);
-
-function compileSCSS() {
-	let stream = gulp.src(dirs.assets.src + '/**/*.scss')
+function styles() {
+	let stream = gulp.src(PATHS.src + FILES.scss)
 		.pipe(sourcemaps.init())
         .pipe(sass()).on('error', sass.logError)
         .pipe(autoprefixer({
         	browsers: ['last 2 versions', 'IE 9']
         }));
 
-    if(prod) {
+    if(PRODUCTION) {
     	let uglify = require('gulp-clean-css');
     	stream.pipe(uglify());
 	} else
 		stream.pipe(sourcemaps.write('./'));
 
-	return stream.pipe(gulp.dest(dirs.assets.dist));
+	return stream.pipe(gulp.dest(PATHS.dest));
 }
 
-gulp.task('compile:styles:scss', compileSCSS);
-
-function compilePUG() {
-	return gulp.src(dirs.assets.src + '/**/*.pug')
+function templates() {
+	return gulp.src(PATHS.src + FILES.pug)
 		.pipe(pug())
-		.pipe(gulp.dest(dirs.assets.dist));
+		.pipe(gulp.dest(PATHS.dest));
 }
 
-gulp.task('compile:templates:pug', compilePUG);
-
-let compile = gulp.series('clean', gulp.parallel(compileES6, compileES6, compilePUG));
-
-gulp.task('compile', compile);
-
-// # Linting tasks
-function lintES6() {
-	let eslint = require('gulp-eslint');
-	return gulp.src([dirs.assets.src + '/**/*.js'])
+// # Linter functions
+function lintScripts() {
+	return gulp.src(PATHS.src + FILES.es6)
         .pipe(eslint())
         .pipe(eslint.format());
 }
 
-gulp.task('lint:javascript:es6', lintES6);
-
-function lintSCSS() {
-	let stylelint = require('gulp-stylelint');
-	return gulp.src([dirs.assets.src + '/**/*.scss'])
+function lintStyles() {
+	return gulp.src(PATHS.src + FILES.scss)
         .pipe(stylelint({
         	failAfterError: false,
 			reporters: [
-				{formatter: 'string', console: true}
+				{
+					formatter: 'string',
+					console: true
+				}
 			]
         }));
 }
 
-gulp.task('lint:styles:scss', lintSCSS);
+// # Watcher functions
+function watch() {
+	gulp.watch(PATHS.src + FILES.scss, gulp.parallel(styles, lintStyles));
+    gulp.watch(PATHS.src + FILES.es6, gulp.parallel(scripts, lintScripts));
+    gulp.watch(PATHS.src + FILES.pug, templates);
+}
 
-let lint = gulp.parallel(lintES6, lintSCSS);
+// Gulp tasks
+exports.clean = clean;
 
-gulp.task('lint', lint);
+let compile = gulp.series(clean, gulp.parallel(scripts, scripts, templates));
+exports.compile = compile;
+
+let lint = gulp.parallel(lintScripts, lintStyles);
+exports.lint = lint;
 
 // # Watcher tasks
-function watchCompile() {
-	gulp.watch(dirs.assets.src + '/**/*.scss', compileSCSS);
-    gulp.watch(dirs.assets.src + '/**/*.js', compileES6);
-    gulp.watch(dirs.assets.src + '/**/*.pug', compilePUG);
-}
-
-gulp.task('watch:compile', gulp.series(compile, watchCompile));
-
-function watchLint() {
-	gulp.watch(dirs.assets.src + '/**/*.scss', lintSCSS);
-    gulp.watch(dirs.assets.src + '/**/*.js', lintES6);
-}
-
-gulp.task('watch:lint', gulp.series(lint, watchLint));
-
-let watch = gulp.parallel(watchCompile, watchLint);
-
-gulp.task('watch', watch);
+exports.watch = watch;
 
 // # Default tasks
 gulp.task('default', gulp.series(compile, watch));
+
+// # User Tasks
